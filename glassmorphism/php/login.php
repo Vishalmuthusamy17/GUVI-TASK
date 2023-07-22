93250a3fb8ac599b1b15484f656f68b2ef1dee01
VISHAL MUTHSAMY <128900249+Vishalmuthusamy17@users.noreply.github.com>
2023-07-22 21:49:11 +0530

    Update login.php

--- glassmorphism/php/login.php
+++ glassmorphism/php/login.php
@@ -28,30 +28,3 @@
 ?>
 
 
-
-<?php
-session_start();
-include('db_connection.php');
-
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = mysqli_real_escape_string($conn,$_POST['username']);
-    $password = mysqli_real_escape_string($conn,$_POST['password']); 
-
-    $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
-    $result = mysqli_query($conn,$sql);
-    $count = mysqli_num_rows($result);
-
-    if($count == 1) {
-        $_SESSION['login_user'] = $username;
-        header("location: dashboard.php");
-    }else {
-        $error = "Your Login Name or Password is invalid";
-    }
-}
-?>
-
-<form action="" method="post">
-    <input type="text" name="username">
-    <input type="password" name="password">
-    <input type="submit" value="Login">
-</form>
